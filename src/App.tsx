cf7ff6f11a0e84917444c2a03b7eef8d7e7d7134
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2026-02-13 22:54:42 -0800

    Add pick problems button
    Co-authored by LLM

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { problems } from './problems'
+
+function openRandomProblems() {
+  const copy = [...problems]
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]]
+  }
+  for (const url of copy.slice(0, 5)) {
+    window.open(url, '_blank')
+  }
+}
+
 function App() {
-  return <>Hello, world!</>
+  return (
+    <>
+      <button onClick={openRandomProblems}>Open 5 random problems</button>
+    </>
+  )
 }
 
 export default App
