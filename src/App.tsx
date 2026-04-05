77983a759ec700bad1d1c3207e032880234eef8e
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2026-04-04 23:00:27 -0700

    Allow opening problems from specific sets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { problems } from './problems'
+import { easyLife, easyKill, koreanProblemAcademy1, problems } from './problems'
 
-function openRandomProblems() {
-  const copy = [...problems]
+function openRandomProblems(problemSet: string[]) {
+  const copy = [...problemSet]
   for (let i = copy.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [copy[i], copy[j]] = [copy[j], copy[i]]
@@ -14,7 +14,21 @@ function openRandomProblems() {
 function App() {
   return (
     <>
-      <button onClick={openRandomProblems}>Open 5 random problems</button>
+      <p>
+      Open 5 random problems from:
+      </p>
+      <div>
+        <button onClick={() => openRandomProblems(easyLife)}>Easy Life</button>
+      </div>
+      <div>
+        <button onClick={() => openRandomProblems(easyKill)}>Easy Kill</button>
+      </div>
+      <div>
+        <button onClick={() => openRandomProblems(koreanProblemAcademy1)}>Korean Problem Academy 1</button>
+      </div>
+      <div>
+        <button onClick={() => openRandomProblems(problems)}>Everything</button>
+      </div>
     </>
   )
 }
