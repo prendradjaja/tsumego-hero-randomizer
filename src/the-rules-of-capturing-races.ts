8d70a80e7758c2f100fd7adba9bc48f6c741f796
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2026-05-07 12:53:40 -0700

    Add set: The Rules of Capturing Races

---
+++ src/the-rules-of-capturing-races.ts
@@ -0,0 +1,12 @@
+import * as raw from "./the-rules-of-capturing-races.raw";
+
+// export const all = [
+//   ...raw.type1,
+//   ...raw.type2,
+//   ...raw.type3Plus,
+// ];
+
+export const upToType2 = [
+  ...raw.type1,
+  ...raw.type2,
+];
