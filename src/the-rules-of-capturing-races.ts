90f6d5c9cda1a3d879b75e000fc629c4c5bdffc7
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2026-07-22 20:58:44 -0700

    Capturing races ++

--- src/the-rules-of-capturing-races.ts
+++ src/the-rules-of-capturing-races.ts
@@ -1,12 +1,15 @@
 import * as raw from "./the-rules-of-capturing-races.raw";
 
-// export const all = [
-//   ...raw.type1,
-//   ...raw.type2,
-//   ...raw.type3Plus,
-// ];
+export const all = [
+  ...raw.type1,
+  ...raw.type2,
+  ...raw.type3Plus,
+];
 
 export const upToType2 = [
   ...raw.type1,
   ...raw.type2,
 ];
+
+export const first140 = all.slice(0, 140);
+
